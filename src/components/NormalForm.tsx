9a671d6fd05612ca57f01f2c021c4dd4db458d98
rajeshpalgithub <rajesh.thinktech@gmail.com>
2021-08-02 14:10:46 +0530

    Basic Validation

---
+++ src/components/NormalForm.tsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import {Button, TextField} from '@material-ui/core'
+
+
+interface IFormValue{
+    name:string;
+    email:string;
+}
+
+function NormalForm() {
+    const initialValue:IFormValue = {
+        name:'',
+        email:''
+    };
+    const [formVal, setFormValue] = useState<IFormValue>(initialValue);
+    const [validationError, setValidationError] = useState<any>({});
+    const validate = (props:any):boolean =>{
+        const target = {
+            name:'',
+            email:''
+        };
+        let isError:boolean = false;
+        if( props.name === ''){
+            target.name = 'Please Enter Name';
+            isError = true;
+        }
+        const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        if(props.email === ''){
+            target.email =  'Please Enter Email';
+            isError = true;
+        }else{
+            console.log(props.email);
+            if(!regExp.test(props.email)){
+                target.email =  'Please Enter Valid Email';
+                isError = true;
+            }
+        }
+        setValidationError({...validationError,...target});
+       
+        return isError;
+    }
+    const handelSubmit =(event:any) =>{
+        event.preventDefault();
+        console.log("validate Return",validationError)
+        if(!validate(formVal))
+        {
+            console.log('Processing Form');
+        }
+    }
+    const handelOnChange =(event:any)=>{
+        const {name,value} = event.target;
+        setFormValue({
+            ...formVal,
+            [name]:value
+        })
+    }
+    
+    return(
+        <form onSubmit={handelSubmit} autoComplete="off">
+            <TextField type="text"
+                name="name" 
+                value={formVal.name} 
+                onChange={handelOnChange} 
+                placeholder="Name"
+                {...(validationError.name && {error:true, helperText:validationError.name})}
+            />
+            <TextField type="text"
+                name="email"
+                value={formVal.email} 
+                onChange={handelOnChange} 
+                placeholder="Email"
+                {...(validationError.email && {error:true, helperText:validationError.email})}
+                
+            />
+            <Button type="submit">Submit</Button>
+
+        </form>
+    );
+}
+
+export default NormalForm;
